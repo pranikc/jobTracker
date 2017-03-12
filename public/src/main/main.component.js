64ce750f0269acd9c274d25b38ce6eaf244b7aac
pranikc <pranikc@gmail.com>
2017-03-12 14:52:58 -0500

    adding more companies to dynamic array of users

--- public/src/main/main.component.js
+++ public/src/main/main.component.js
@@ -3,15 +3,39 @@
     app.MainComponent = ng.core
         .Component({
             selector: 'mainInfo',
-            templateUrl: './src/main/main.html'
+            templateUrl: './src/main/main.html',
+            providers: [ng.http.HTTP_PROVIDERS, app.MainService]
         }).Class({
-            constructor: function () {
-                this.newJob = {}
-            },
+            constructor: [app.MainService, function(_service){
+                this.service = _service;
+                this.newJob = {};
+                this.jobsList = [];
+            }],
 
             apply: function(){
                 this.newJob.date = new Date();
                 console.log(this.newJob);
+                this.newJob.name = this.newJob.name.trim().toLowerCase();
+                this.service.addUser(this.newJob)
+                    .subscribe(this.getListOfCompanies.bind(this), this.updateError.bind(this));
+            },
+            getListOfCompanies: function()
+            {
+                this.service.getAllUsers(this.newJob)
+                    .subscribe(this.getData.bind(this), this.updateError.bind(this));
+            },
+            getData: function(data)
+            {
+                this.jobsList = data;
+                console.log(this.jobsList);
+            },
+            updateError: function(err)
+            {
+                console.log(err);
+            },
+            ngOnInit: function()
+            {
+                this.getListOfCompanies();
             }
         });
 }) (window.app || (window.app = {}));/**
